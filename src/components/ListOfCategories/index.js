ff7f1bde575a66aac4e253bb476caf6006a00d10
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-02 14:39:12 -0500

    added changes

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+
+function useCategoriesData () {
+    const [categories, setCategories] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        window.fetch('https://petsgram-server.now.sh/categories')
+            .then(res => res.json())
+            .then(res => {
+                setCategories(res)
+                setLoading(false)
+            })
+    }, [])
+    return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-    return (
-        <List>
-            {categories.map(({ id, ...category }) => (
+    const { categories, loading } = useCategoriesData()
+    const [showFixed, setShowFixed] = useState(false)
+
+    useEffect(() => {
+        function onScroll (e) {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+        document.addEventListener('scroll', onScroll)
+
+        return () => {
+            document.removeEventListener('scroll', onScroll)
+        }
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List fixed={fixed}>
+            {loading ? (
+                <Item key='loading'>
+                    <Category />
+                </Item>
+            ) : (categories.map(({ id, ...category }) => (
                 <Item key={id}>
                     <Category {...category} />
                 </Item>
-            ))}
+            )))}
         </List>
+    )
+
+    return (
+        <>
+            {renderList()}
+            {showFixed && renderList(showFixed)}
+        </>
     )
 }
